b3e793d06680aa4c48984b92f46463d91263a4cd
fivitti <fivitti@gmail.com>
2014-07-10 23:15:24 +0200

    Fix Ertilp kernel. (From Sertilp.) Alpha 2.

--- Source/SMDV_PyCUDA/cudaAgregator.py
+++ Source/SMDV_PyCUDA/cudaAgregator.py
@@ -223,6 +223,119 @@ def getSertilpCudaCode(shDot_size = 0, threadPerRow = 2, sliceSize = 32, prefetc
     
     return tpl
 
+def getErtilpCudaCode_old(block_sice, threadPerRow, prefetch):
+    tpl = '''
+        texture<float,1,cudaReadModeElementType> labelsTexRef;
+
+        __device__ void SpMV_ERTILP(const float * vals,
+        									   const int * colIdx, 
+        									   const int * rowLength,
+        									   const int row,
+        									   const int rowsB,
+        									   const int shRows,
+        									   volatile float* shDot)
+        {
+        
+        	const int tid = threadIdx.x; // index in block
+        	const int idxR = tid/{{ THREADS_ROW }}; //row index mapped into block region
+        	const int idxT = tid%{{ THREADS_ROW }}; // thread number in Thread Group
+        
+        
+        
+        	float preVals[{{ PREFETCH_SIZE }}];
+        	int preColls[{{ PREFETCH_SIZE }}];
+        
+        	float dot[{{ PREFETCH_SIZE }}]={0};
+        
+        	int maxEl = rowLength[row]; //original row length divided by T*PREFETCH
+        
+        	unsigned int j=0;
+        	unsigned int arIdx=0;
+        
+        	for(int i=0; i<maxEl;i++)
+        	{
+        		
+        		#pragma unroll
+        		for( j=0; j<{{ PREFETCH_SIZE }};j++)			
+        		{
+        			arIdx = (i*{{ PREFETCH_SIZE }}+j)*shRows*{{ THREADS_ROW }}+row*{{ THREADS_ROW }}+idxT;
+        			preColls[j]=colIdx[arIdx];
+        			preVals[j]=vals[arIdx];
+        		}
+        		
+        		#pragma unroll
+        		for( j=0; j<{{ PREFETCH_SIZE }};j++){
+        			dot[j]+=preVals[j]*tex1Dfetch(labelsTexRef,preColls[j]);
+        		}
+        	}
+        	
+        	#pragma unroll
+        	for( j=1; j<{{ PREFETCH_SIZE }};j++){
+        		dot[0]+=dot[j];
+        	}
+        
+        	//__syncthreads();	
+        
+        	// special indexing, values for example for T=4 BlockSize=256
+        	//for row=0 values are stored on position 0,64,128,192 
+        	//for row=1 values are stored on position 1,65,129,193 ...
+        	shDot[idxT*rowsB+idxR]=dot[0];
+        
+        	__syncthreads();		
+        
+        	//reduction to some level
+        	for( j=blockDim.x/2; j>=rowsB; j>>=1) //s/=2
+        	{
+        		if(tid<j){
+        			shDot[tid]+=shDot[tid+j];
+        		}
+        		__syncthreads();
+        	}
+        
+        
+        }
+        
+        extern "C" __global__ void rbfERTILP(const float * vals,
+        									   const int * colIdx, 
+        									   const int * rowLength, 
+        									   float * results,
+        									   const int num_rows)
+        {
+        	__shared__ int shRows;
+        	__shared__ float shDot[{{ BLOCK_SIZE }}];
+        	shDot[threadIdx.x]=0.0;	
+        
+        	if(threadIdx.x==0)
+        	{
+        		shRows = num_rows;
+        	}
+        	__syncthreads();
+        
+        
+        	//const int idx  = blockDim.x * blockIdx.x + threadIdx.x;  // global thread index
+        	int row  = (blockDim.x * blockIdx.x + threadIdx.x)/{{ THREADS_ROW }};
+        
+        	const int rowsB= blockDim.x/{{ THREADS_ROW }} ;//{{ BLOCK_SIZE }}/{{ THREADS_ROW }};  //rows in block
+        	//#define rowsB {{ BLOCK_SIZE }}/{{ THREADS_ROW }}
+        
+        	if(row<shRows)
+        	{
+        
+        		SpMV_ERTILP(vals,colIdx,rowLength,row,rowsB,shRows,shDot);
+        		//if(row2<shRows){
+        		if(threadIdx.x<rowsB){
+        			//results[row2]=row2;			
+        			unsigned int row2=blockIdx.x* rowsB+threadIdx.x;
+         			if(row2<shRows)
+        				results[row2]=shDot[threadIdx.x];
+        		}
+        	}//if row<nrRows	
+        
+        }
+        '''
+    tpl = convertString(tpl, BLOCK_SIZE = block_sice, THREADS_ROW = threadPerRow, PREFETCH_SIZE = prefetch)
+    return tpl
+    
 def getErtilpCudaCode(block_sice, threadPerRow, prefetch):
     tpl = '''
         texture<float,1,cudaReadModeElementType> labelsTexRef;
