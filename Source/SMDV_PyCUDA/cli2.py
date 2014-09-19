9e52f55e31451a893c0e0470c51527dc91067696
fivitti <fivitti@gmail.com>
2014-09-19 15:58:08 +0200

    Extended compensation

--- Source/SMDV_PyCUDA/cli2.py
+++ Source/SMDV_PyCUDA/cli2.py
@@ -138,7 +138,7 @@ def conv(ctx, block, ss, tpr, align, prefetch, ell, sle, see, ert):
 @click.option('-avr', '--avrtime', is_flag=True, help='Print average time multiplication')
 @click.option('-std', '--standard-deviation', 'std', is_flag=True, help='Print standard deviation of time multiplication')
 @click.option('--test', flag_value=0.0005, help='Testing result multiplication. Print bad row. Value is confidence factor.')
-@click.option('-com', '--compensate', 'com', is_flag=True, help='First time is remove (returned times decremented by one). Recommended in testing the speed, because the first time is a long delay.' )
+@click.option('-com', '--compensate', 'com', flag_value=1, help='N first time are remove (returned times decremented by n). Recommended in testing the speed, because the n first times (e. g. one) are a long delay.' )
 
 @click.option('-o', '--output', type=click.File(mode='a', lazy=True), help='File to save raport. Format CSV. If exist append new data.')
 
@@ -190,7 +190,9 @@ def multiply(ctx, block, ss, tpr, align, prefetch, ell, sle, see, ert, cpu, repe
         if test: testResult(resultNumpy, resultMultiply[0], test, quite, lang)
          
 def resumeResult(ctx, resultMuliply, resultPrint, timePrint, avrTimePrint, stdTimePrint, quite, lang, output, formatName, compensate):
-    times = resultMuliply[1] if not compensate else resultMuliply[1][1:]
+    times = resultMuliply[1]
+    if compensate:
+        times = times[compensate:]
     if resultPrint:
         click.echo(('' if quite else getMessage('result', lang)) + str(resultMuliply[0]))
     if timePrint:
