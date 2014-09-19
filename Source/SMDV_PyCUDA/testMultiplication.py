e6e1e6524788ca5847c6ee77ca2cd582cc4193b2
fivitti <fivitti@gmail.com>
2014-09-19 20:02:18 +0200

    Test

--- Source/SMDV_PyCUDA/testMultiplication.py
+++ Source/SMDV_PyCUDA/testMultiplication.py
@@ -35,7 +35,7 @@ def rowEqualsIgnoreEndZeroGetRows(a, b, procentUfnosci = 0.05):
 
 if __name__ == '__main__':
     ### Stałe programu ###
-#    plikMacierzy = 'Macierz_8x8.mtx'
+    plikMacierzy = 'Macierz_8x8.mtx'
 #    plikMacierzy = 'Macierz_9x9.mtx'
 #    plikMacierzy = 'Macierz_128x128.mtx'
 #    plikMacierzy = 'Macierz_2048x2048_2.mtx'
@@ -44,12 +44,12 @@ if __name__ == '__main__':
 #    plikMacierzy = 'dw8192.mtx'
 #    plikMacierzy = 'Macierz_int_9x9.mtx'
 #    plikMacierzy = 'Macierz_float_128x128.mtx'
-    plikMacierzy = 'Macierz_int_65x65.mtx'
+#    plikMacierzy = 'Macierz_int_65x65.mtx'
     
 #    folderMacierzy = "E:\Slawek\SMVD\SMDV\Macierze\\"
 #    folderMacierzy = "E:\Slawek\SMVD\SMDV\Macierze\\wygenerowane\\"
 #    folderMacierzy = "..\\..\\Matrices\\Generated\\"
-    folderMacierzy = "../../Matrices/Generated/"
+    folderMacierzy = "../../Data/Generated/"
     
     blockSize = 32
     sliceSize =64 # 64 128 
@@ -60,34 +60,35 @@ if __name__ == '__main__':
     dokladnoscCzasu = 3
     ###
     macierz = scipy.io.mmread(folderMacierzy + plikMacierzy)
+    wektor = numpy.arange(1, macierz.shape[1]+1, dtype=numpy.float32)
 ##    from math import ceil
 ##    conv = convertToSertilpELL(macierz, watkiNaWiersz=threadPerRow, sliceSize=sliceSize, align=int(ceil((sliceSize*threadPerRow*1.0)/alignStala)*alignStala), prefetch=prefetch)
 ##    macierz = numpy.array([[0, 0, 3], \
 ##                           [0, 0, 2], \
 ##                           [0, 0, 1]])
 #    
-#    resultELLPack = multiplyELL(macierz, blockSize=blockSize, repeat=powtorzenia)
+    resultELLPack = multiplyELL(macierz, vector=wektor, blockSize=blockSize, repeat=powtorzenia)
 #    print "Czas ELL: " + str(round(resultELLPack[1], dokladnoscCzasu))
 #    resultSlicedELLPack = multiplySlicedELL(macierz, alignStala, sliceSize, threadPerRow, repeat=powtorzenia)
 #    print "Czas SlicedELL: " + str(round(resultSlicedELLPack[1], dokladnoscCzasu))
 #    resultSertilpELLPack = multiplySertilp(macierz, alignStala, sliceSize, threadPerRow, prefetch=prefetch, repeat=powtorzenia)
 #    print "Czas SertilpELL: " + str(round(resultSertilpELLPack[1], dokladnoscCzasu))
-    resultCPU = multiplyCPU(macierz, repeat=powtorzenia)
-    resultErtilp = multiplyErtilp(macierz, threadPerRow, prefetch, blockSize=blockSize, repeat=powtorzenia)
+    resultCPU = multiplyCPU(macierz, vector=wektor, repeat=powtorzenia)
+#    resultErtilp = multiplyErtilp(macierz, vector=wektor, threadPerRow=threadPerRow, prefetch=prefetch, blockSize=blockSize, repeat=powtorzenia)
 #    print "Czas CPU: " + str(round(resultCPU[1], dokladnoscCzasu))     
 #    print "Czy poprawnie ELL - SlicedELL? " + str(rowEqualsIgnoreEndZero(resultELLPack[0], resultSlicedELLPack[0]))
 #    print "Czy poprawnie CPU - ELL? " + str(rowEqualsIgnoreEndZero(resultELLPack[0], resultCPU[0]))
 #    print "Czy poprawnie ELL - SertilpELL? " + str(rowEqualsIgnoreEndZero(resultELLPack[0], resultSertilpELLPack[0]))
 #    print "Czy poprawnie SertilpELL - CPU? " + str(rowEqualsIgnoreEndZero(resultSertilpELLPack[0], resultCPU[0]))
-#    rE = resultELLPack[0]
+    rE = resultELLPack[0]
     rC =  resultCPU[0]
 #    rSl = resultSlicedELLPack[0]
 #    rS =  resultSertilpELLPack[0]
-    rEr =resultErtilp[0]
+#    rEr =resultErtilp[0]
 #    print u"Maksymalny błąd względny ELL - CPU: " + str(max(numpy.abs(rE - rC) / abs(rE + rC))*100)
 #    print u"Maksymalny błąd względny SertlipELL - CPU: " + str(max(numpy.abs(rS - rC) / abs(rS + rC))*100)
 #    print u"Maksymalny błąd względny SertlipELL - ELL: " + str(max(numpy.abs(rE - rS) / abs(rE + rS))*100)
-    bledneWiersze = rowEqualsIgnoreEndZeroGetRows(rC, rEr)
+    bledneWiersze = rowEqualsIgnoreEndZeroGetRows(rC, rE)
     print bledneWiersze
     
 #    for i in range(32, 68):
