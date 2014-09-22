0cc3a59c327505eb30f66e1751c1370ff67b4ae5
fivitti <fivitti@gmail.com>
2014-09-22 17:06:31 +0200

    Edit display method.

--- cli-conv.py
+++ cli-conv.py
@@ -34,29 +34,61 @@ colors = {
 def cli(block, ss, tpr, align, prefetch, ell, sle, see, ert, quite, matrix_paths):
     paths = map(str, matrix_paths)
     matrices = sortPaths(pathReduction(paths), '.mtx')['.mtx']
-    lang = 'en'
     for matrixPath in matrices:
-        if not quite: click.secho(getMessage('conv', lang), fg=colors['success'])
+        if not quite: click.secho(getMessage('conv'), fg=colors['success'])
         try:
             matrix = scipy.io.mmread(matrixPath)
         except:
             click.secho(getMessage('open_failed') % matrixPath, fg=colors['danger'])
             continue
-        stringListInList = lambda listInList: '\n'.join(map(str, listInList))
         if ell:
-            if not quite: click.secho(getMessage('convEll', lang), fg=colors['warning'])
-            click.echo(stringListInList(convertToELL(matrix, array=False)))
+            if not quite: click.secho(getMessage('convEll'), fg=colors['warning'])
+            printFormat(convertToELL(matrix, array=False))
         if sle:
-            if not quite: click.secho(getMessage('convSliced', lang), fg=colors['warning'])
-            click.echo(stringListInList(convertToSlicedELL(matrix, array=False, watkiNaWiersz=tpr, sliceSize=ss, align=align)))
+            if not quite: click.secho(getMessage('convSliced'), fg=colors['warning'])
+            printFormat(convertToSlicedELL(matrix, array=False, watkiNaWiersz=tpr, sliceSize=ss, align=align))
         if see:
-            if not quite: click.secho(getMessage('convSertilp', lang), fg=colors['warning'])
-            click.echo(stringListInList(convertToSertilpELL(matrix, array=False, watkiNaWiersz=tpr, sliceSize=ss, align=align, prefetch=prefetch)))
+            if not quite: click.secho(getMessage('convSertilp'), fg=colors['warning'])
+            printFormat(convertToSertilpELL(matrix, array=False, watkiNaWiersz=tpr, sliceSize=ss, align=align, prefetch=prefetch))
         if ert:
-            if not quite: click.secho(getMessage('convErtilp', lang), fg=colors['warning'])
-            click.echo(stringListInList(convertToErtilp(matrix, threadPerRow=tpr, prefetch=prefetch, array=False)))
+            if not quite: click.secho(getMessage('convErtilp'), fg=colors['warning'])
+            printFormat(convertToErtilp(matrix, threadPerRow=tpr, prefetch=prefetch, array=False))
 
-def getMessage(idMessage, lang='en'):
+def printFormat(convertedMatrix):
+    formatFirstRow = '{0:<7}{1:>18}{2:>18}'
+    formatSecondRow = '{0:<7}{1:>12}'
+    formatTable = '{0:<50}{1:>}'
+    rows = zip(convertedMatrix[0], convertedMatrix[1])
+    first = [click.style(formatFirstRow.format('Idx', 'Value', 'Column'), fg=colors['info']), ]
+    for idx, val in enumerate(rows):
+        if val[0]:
+            first.append(formatFirstRow.format(idx, val[0], val[1]))
+    second = [click.style(formatSecondRow.format('       Row', '      Row length'), fg=colors['info'])]     
+    for idx, val in enumerate(convertedMatrix[2]):
+        if val:
+            second.append(formatSecondRow.format(idx, val))
+        
+    if len(convertedMatrix) > 3:
+            second.append('')
+            second.append(click.style(formatSecondRow.format('Slice', 'Slice size'), fg=colors['info']))
+            for idx, val in enumerate(convertedMatrix[3]):
+                second.append(formatSecondRow.format(idx, val))
+                
+    lenFirst = len(first)
+    lenSecond = len(second)
+    if lenFirst > lenSecond:
+        second += [''] * (lenFirst - lenSecond)
+    else:
+        first += [''] * (lenSecond - lenFirst)
+    
+    table = zip(first, second)
+    for left, right in table:
+        click.echo(formatTable.format(left, right))
+    
+        
+        
+        
+def getMessage(idMessage):
     return {'conv' : u'Representations of the matrix after conversion to selected formats: ',
             'convEll' : 'Conversion to ELLPACK',
             'convSliced' : 'Conversion to SLICED ELLPACK',
