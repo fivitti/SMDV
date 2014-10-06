2bdb269ec18afb9b758d8ca98411265e68e57ad1
fivitti <fivitti@gmail.com>
2014-10-06 17:36:15 +0200

    14

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -267,8 +267,11 @@ def multiplyErtilp(macierz, vector, threadPerRow = 2, prefetch = 2, blockSize = 
     if True:
         mac = transform_to_ertilp(macierz, prefetch=prefetch, threads_per_row=threadPerRow)
 #        rowLength = cuda.to_device(mac[2])
+        print mac[2]
 #        rowLength = cuda.to_device(numpy.array([int(ceil((i+0.0)/(threadPerRow*prefetch))) for i in mac[2]]))
-        rowLengthTemp = numpy.array(numpy.ceil(numpy.array(mac[2]) / (threadPerRow*prefetch)), dtype=numpy.int32)
+        print numpy.ceil(numpy.array(mac[2]) / (4))
+        rowLengthTemp = numpy.array(numpy.ceil(mac[2] / (threadPerRow*prefetch)), dtype=numpy.int32)
+        print rowLengthTemp
         rowLength = cuda.to_device(rowLengthTemp)
 #    else:
 #        mac = convert_to_ertilp(macierz, threads_per_row=threadPerRow, prefetch=prefetch)
