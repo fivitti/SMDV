5f49ef7729c2bfaab51ec45a3b542a2565739a1f
fivitti <fivitti@gmail.com>
2014-10-12 23:27:52 +0200

    Rename module. Pylint 10/10!

--- filesutilites.py
+++ filesutilites.py
@@ -3,27 +3,27 @@
 Created on Sun Sep 21 19:08:53 2014
 @author: Slawomir Figiel
 
-Module with the methods working on file paths/
+Module with the methods working on file paths.
 """
 from os.path import isfile, join, isdir, splitext
 from os import listdir
 
 def path_reduction(paths):
     '''
-    Method process paths. If in paths is directory, method searches it 
-    for the files. Returns list of files path with paths and files in 
+    Method process paths. If in paths is directory, method searches it
+    for the files. Returns list of files path with paths and files in
     directories.
-    
+
     Parameters
     ==========
     paths : enumerate
         list of files
-    
+
     Returns
     =======
     paths : list
         list of path files in paths and files in directory in paths
-        
+
     Examples
     ========
     >>> paths = [data/file1.txt, data/file2.pdf, dir/]
@@ -35,27 +35,27 @@ def path_reduction(paths):
         if isdir(path):
             files.extend(files_in_dir(path))
         else:
-            files.append(path) 
+            files.append(path)
     return files
 
 def sort_paths(paths_files, *extensions):
     '''
-    Method sorts paths after file extension and returns a dictionary whose 
+    Method sorts paths after file extension and returns a dictionary whose
     keys are the extension and the values list of path.
-    
+
     Parameters
     ==========
     paths_files : list paths files
         paths to sort
     *extensions : strings
         extensions that will be included in the dictionary
-    
+
     Returns
     =======
     sort paths : dict
         dictionary whose  keys are the extension and values list of
         path
-    
+
     Examples
     ========
     >>> paths = ['dir/file1.txt', 'file2.pdf', 'dir/file3.txt',
@@ -63,29 +63,29 @@ def sort_paths(paths_files, *extensions):
     >>> sort_paths(paths, 'txt', 'pdf')
         {'txt' : ['dir/file1.txt', 'dir/file3.txt'], 'pdf' : [file2.pdf]}
     '''
-    dictExtFiles = dict((ext, []) for ext in extensions)
-    dictKeys = dictExtFiles.keys()
+    dict_ext_files = dict((ext, []) for ext in extensions)
+    dict_keys = dict_ext_files.keys()
 
-    for f in paths_files:
-        ext = splitext(f)[1]
-        if ext in dictKeys:
-            dictExtFiles[ext].append(f)       
-    return dictExtFiles
-    
+    for file_ in paths_files:
+        ext = splitext(file_)[1]
+        if ext in dict_keys:
+            dict_ext_files[ext].append(file_)
+    return dict_ext_files
+
 def files_in_dir(dir_path):
     '''
     Method return full path files in directory.
-    
+
     Parameters
     ==========
     dir_path : string
         directory
-        
+
     Returns
     =======
     files paths : list
         List of full files path in directory.
-        
+
     Examples
     ========
     >>> files_in_dir(dir)
@@ -96,4 +96,3 @@ def files_in_dir(dir_path):
 
 if __name__ == '__main__':
     pass
-    
